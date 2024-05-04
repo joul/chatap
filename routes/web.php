95c78844e1f74f5b5ada749a634b5f23a710768d
Joul <joul84@gmail.com>
2024-05-04 16:38:57 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
+
+//User related routes
+Route::get('/', [UserController::class, "showCorrectHomepage"]);
+
+Route::post('/register', [UserController::class, "register"]);
+
+Route::post('/login', [UserController::class, "login"]);
+
+Route::post('/logout', [UserController::class, "logout"]);
+
+
+//Blog posts related routes
+Route::get('/create-post',[PostController::class, "showcreateForm"]);
